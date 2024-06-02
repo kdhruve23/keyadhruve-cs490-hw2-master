6fe41f78b61260b5bb9cff4ac8b8313666802021
kdhruve23 <63871827+kdhruve23@users.noreply.github.com>
2024-06-02 11:50:49 -0400

    Add files via upload

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,13 @@
+//import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+      <p>Site created to demonstrate mastery of redwood.</p>
+    </>
+  )
+}
+
+export default AboutPage
